899a28136bc80487621a5479cc8327ed7c452add
Sergey Shulga <shul.sergio@gmail.com>
2024-04-15 21:01:53 +0300

    v.9.2 add js to modal

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-const a = 1;
-const b = 2;
+const a = 10000;
+const b = 20000;
 const c = a + b;
 console.log(c);
+// const result = document.querySelector('.hero-text').innerHTML;
+const OpenModalMob = document.querySelector('.btn-one');
+const modal = document.querySelector('.backdrop');
+const CloseMob = document.querySelector('.close-btn');
+OpenModalMob.onclick = function () {
+  modal.classList.add('is-open');
+};
+CloseMob.onclick = function () {
+  modal.classList.remove('is-open');
+};
+
+const OpenModalTab = document.querySelector('.btn-two');
+const CloseTab = document.querySelector('.close-big-btn');
+OpenModalTab.onclick = function () {
+  modal.classList.add('is-open');
+};
+CloseTab.onclick = function () {
+  modal.classList.remove('is-open');
+};
+const Closeall = document.querySelector('.navigation-list');
+Closeall.onclick = function () {
+  modal.classList.remove('is-open');
+};
